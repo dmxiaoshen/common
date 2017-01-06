354bb3337741d83e09fa7babe3b85263bbccae6f
hushengen <hzhsg@tairanchina.com>
2017-01-06 18:57:01 +0800

    add client

---
+++ common-client/src/main/java/com/bell/common/client/App.java
@@ -0,0 +1,13 @@
+package com.bell.common.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
