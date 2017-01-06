4caf4490b1c6501f70c7b0e0a989fde15b777c73
hushengen <hzhsg@tairanchina.com>
2017-01-06 16:38:35 +0800

    init

---
+++ common-base/src/main/java/com/bell/common/base/App.java
@@ -0,0 +1,14 @@
+package com.bell.common.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.err.println("s".equals(null));
+    }
+}
