24705ab39fad9948652890d36f117ef4e40c201f
dmxiaoshen <dmxiaoshen@163.com>
2017-05-14 02:48:22 +0800

    idea test

--- common-rest/src/main/java/com/bell/common/rest/resource/AbstractResource.java
+++ common-rest/src/main/java/com/bell/common/rest/resource/AbstractResource.java
@@ -5,4 +5,9 @@ import org.slf4j.LoggerFactory;
 
 public abstract class AbstractResource {
 	protected Logger logger = LoggerFactory.getLogger(getClass());
+
+	protected  String say(){
+		System.out.println("say");
+		return null;
+	}
 }
